ad800c174132464004b147a0604161a8338d07ab
Guilherme Urbano Moreira Paiva <guilherme_paiva@yahoo.com>
2022-08-24 21:06:15 -0300

    update 2 da pratica 1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,12 @@
+// Declarando variáveis
 let nome
 let email
 let profissao
 let idade
 let viagemDosSonhos
+// Atribuindo valor das variáveis
+nome = "Guilherme"
+email = "guilherme_paiva@yahoo.com"
+profissao = "Engenheiro"
+idade = 42
+viagemDosSonhos = "Itália"
